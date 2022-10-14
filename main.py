be1015bc5fc120136a016bd835a2ef57ab46666e
rodrigojgq <rodrigojgq@gmail.com>
2022-10-14 00:23:38 -0400

    Add endpoint with request body and the users model

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+# Python
+from typing import Optional
+# Pydantic
+from pydantic import BaseModel
+#Fast API
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+
+class User(BaseModel):
+  first_name: str
+  last_name: str
+  age: int
+  hair_color: Optional[str] = None
+  is_married: Optional[bool] = None
 
 @app.get("/")
 def home():
   return {"Hello": "World"}
+
+# Request and Response Body
+@app.post("/user/new")
+def create_user(user: User = Body(...)):
+  return user
