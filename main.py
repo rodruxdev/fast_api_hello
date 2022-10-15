b2590b4122c58bd409406dfe813adb0caa7c8a31
rodrigojgq <rodrigojgq@gmail.com>
2022-10-14 22:31:46 -0400

    Add endpoint to get user with id validation
    Add descriptions and titles for endpoints.

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from typing import Optional
 from pydantic import BaseModel
 #Fast API
 from fastapi import FastAPI
-from fastapi import Body
-from fastapi import Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -31,7 +30,28 @@ def create_user(user: User = Body(...)):
 
 @app.get("/user/detail/")
 def show_user(
-  name: Optional[str] = Query(None, min_length=1, max_length=50),
-  age: str = Query(...)
+  name: Optional[str] = Query(
+    None,
+    min_length=1,
+    max_length=50,
+    title="User Name",
+    description="This is the user's name. It's between 1 and 50 characters"
+    ),
+  age: str = Query(
+    ...,
+    title="User Age",
+    description="This is the person age. It's required"
+    )
 ):
   return {name: age}
+
+@app.get("/user/detail/{user_id}")
+def show_user(
+  user_id: int = Path(
+    ...,
+    gt=0,
+    title="User ID",
+    description="This is the user ID, It's a required integer and grater than 0"
+    )
+):
+  return {user_id: "It exists!"}
