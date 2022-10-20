63c2880588db812c3ae8c2635b92d5297a3a9da0
rodrigojgq <rodrigojgq@gmail.com>
2022-10-19 23:07:25 -0400

    Add post image endpoint

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pydantic import EmailStr
 #Fast API
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, File, UploadFile
 
 app = FastAPI()
 
@@ -181,3 +181,17 @@ def contact(
   ads: Optional[str] = Cookie(default=None),
 ):
   return {"user_agent": user_agent, "ads": ads}
+
+# Files
+
+@app.post(
+  path="/post-image"
+)
+def post_image(
+  image: UploadFile = File(...)
+):
+  return {
+    "Filename": image.filename,
+    "Format": image.content_type,
+    "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+  }
